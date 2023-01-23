6ce3bdbe40870c27cc1fd5a1dc798ab2829a0edb
samanfathnazarian <saman.nazarian17@gmail.com>
2023-01-23 07:50:05 -0600

    Added custom classes to the Tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   // "**" specifies that the search should include all subdirectories
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+    },
   },
   plugins: [],
 };
